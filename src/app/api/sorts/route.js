bea27c21c647ae61b90aeb1bf5f22f80cd566cb6
ahmed-anchor <ahmedashrafodi.4@gmail.com>
2024-12-15 01:29:04 +0200

    asdasd

--- src/app/api/sorts/route.js
+++ src/app/api/sorts/route.js
@@ -3,6 +3,26 @@ import connectDB from "../../../../config/database";
 import { NextResponse } from "../../../../node_modules/next/server";
 
 
+export async function POST(req) {
+    try {
+        const { sortProduct } = await req.json();
+        await connectDB();
+        console.log(sortProduct)
+        const data = await DashboardModel.find({ sort: sortProduct });
+        return NextResponse.json({ data }, {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'private, max-age=120'
+            },
+        });
+    } catch (error) {
+        return NextResponse.json({ message: 'Internal Server Error' }, {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    };
+  };
 
 export async function GET () {
     try {
