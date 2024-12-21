a99122dcfdf05ab383b82c622c4f0b1d1f3c26a7
ahmed-anchor <ahmedashrafodi.4@gmail.com>
2024-12-21 02:22:34 +0200

    another adsense to commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,6 +25,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <meta name="google-adsense-account" content="ca-pub-7818225422285491" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7818225422285491"
+     crossorigin="anonymous"></script>
       </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-white`}
